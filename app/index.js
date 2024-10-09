0b553190cd783da7fcce0fbf3b545f03f3e31501
Thong Nguyen <giathong3012005@gmail.com>
2024-10-08 23:12:32 -0600

    Lab2-Mobile App

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import App from '../component/App';
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <App />
       </View>
     </View>
   );
@@ -23,12 +23,7 @@ const styles = StyleSheet.create({
     maxWidth: 960,
     marginHorizontal: "auto",
   },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+  
 });
+
+
